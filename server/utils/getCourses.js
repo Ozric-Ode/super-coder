17e16ce0e2a2001f436f1d16ed0296fb57d57cb4
batflarrow <dwivedia2001@gmail.com>
2021-04-24 23:32:56 +0530

    final commit

--- server/utils/getCourses.js
+++ server/utils/getCourses.js
@@ -14,6 +14,21 @@ const getCourses = async() => {
         return false;
     }
 };
+const getCourse = async(Student_Id) => {
+    try {
+        console.log("Student_Id", Student_Id)
+        const pool = await dbFunction.connectToDb();
+        let query = "SELECT * FROM enrolls where Student_Id = ?";
+        const studentCourseRes = await pool.query(query,[Student_Id]);
+        console.log(studentCourseRes[0]);
+        const course = studentCourseRes[0];
+        await dbFunction.disconnectFromDb(pool);
+        return course
+    } catch (error) {
+        console.log(error)
+        return false;
+    }
+};
 
 
-module.exports = getCourses;
+module.exports = {getCourses,getCourse};
